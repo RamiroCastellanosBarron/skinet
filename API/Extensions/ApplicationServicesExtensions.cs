d99b64e75e1a77347da4709597e041d86f0f1405
RamiroCastellanosBarron <ramiro@castellanosbarron.com>
2023-02-16 15:12:59 -0600

    End of section 15

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -2,6 +2,7 @@ using API.Errors;
 using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
@@ -23,6 +24,7 @@ namespace API.Extensions
             });
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ITokenService, TokenService>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ApiBehaviorOptions>(opt =>
--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 `skinet/`
 
 `docker-compose up --detach`
+
+`dotnet-ef migrations add IdentityInitial -p Infrastructure -s API -c AppIdentityDbContext -o Identity/Migrations`
+
+-p --> Project
+
+-s --> Startup
+
+-c --> Context
+
+-o --> Output
